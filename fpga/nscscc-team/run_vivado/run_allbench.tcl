f1c0564908daf97c3b65ed012536f5a698b001f2
ffshff <747293503@qq.com>
2024-08-13 12:39:54 +0800

    modify run_allbench.tcl

--- fpga/nscscc-team/run_vivado/run_allbench.tcl
+++ fpga/nscscc-team/run_vivado/run_allbench.tcl
@@ -47,3 +47,43 @@ run all
 file copy -force ../../software/perf/obj/stringsearch/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
 restart
 run all
+
+file copy -force ../../software/perf/obj/fireye_A0/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/fireye_B2/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/fireye_C0/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/fireye_D1/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/fireye_I2/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/inner_product/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/lookup_table/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/loop_induction/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/my_memcmp/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/minmax_sequence/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
